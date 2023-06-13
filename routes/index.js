44e2a32b1f2bbe190eb9fad6cf4b4c0b9e172269
AsianJack <lucastanakinha100@hotmail.com>
2023-06-13 18:09:46 -0300

    Contato e Login

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+router.get('/contato', function(req, res, next) {
+  res.render('contato');
 });
 
 module.exports = router;
